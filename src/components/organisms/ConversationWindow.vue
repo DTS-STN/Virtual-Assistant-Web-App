b99803be9cfe2b4b9871289c0c4abde6c6ed7fc3
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-08-13 13:55:40 -0400

    Close to a fix for scroll.

--- src/components/organisms/ConversationWindow.vue
+++ src/components/organisms/ConversationWindow.vue
@@ -78,7 +78,7 @@ import ConversationFooter from "../atoms/ConversationFooter.vue";
 import MessageHeader from "../atoms/MessageHeader.vue";
 import TextInput from "../atoms/TextInput.vue";
 import { useStore } from "vuex";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import icons from "../../assets/icons.js";
 export default {
   name: "ConversationWindow",
@@ -151,6 +151,11 @@ export default {
     function resetConversationWindow() {
       tabbable.value = -1;
     }
+
+    watch(chatMessage, (messages, prevMessages) => {
+      const test = document.querySelector("#conversationWindow");
+      test.scrollTop = test.scrollHeight;
+    });
 
     return {
       chatMessage,
