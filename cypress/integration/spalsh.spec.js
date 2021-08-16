747c6967ed16b69c4f46c937ed9c6451797c15cc
shewood <shewood01@gmail.com>
2021-08-16 18:48:12 -0400

    added cypress

---
+++ cypress/integration/spalsh.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/// <reference types="Cypress" />
+
+describe("home page", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    //   cy.injectAxe();
+    });
+  
+    it("landing page loaded", () => {
+      cy.url().should("contains", "/");
+    });
+  
+
+  });
