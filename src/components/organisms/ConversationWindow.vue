7ff4657133cb10277b486513078d42c55294b22d
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-08-17 15:46:19 -0400

    dynamically create timestamps based on oldest message received

--- src/components/organisms/ConversationWindow.vue
+++ src/components/organisms/ConversationWindow.vue
@@ -33,8 +33,17 @@
         aria-live="polite"
       >
         <li>
-          <p class="text-center font-heading text-sm font-light text-gray-dark">
-            {{ $t("messageTime") }}
+          <p
+            class="
+              text-center
+              uppercase
+              font-heading
+              text-sm
+              font-light
+              text-gray-dark
+            "
+          >
+            {{ messageTimestamp || $t("messageTime") }}
           </p>
         </li>
         <ConversationMessage
@@ -80,6 +89,7 @@ import TextInput from "../atoms/TextInput.vue";
 import { useStore } from "vuex";
 import { computed, ref, watch, nextTick } from "vue";
 import icons from "../../assets/icons.js";
+import { useI18n } from "vue-i18n";
 export default {
   name: "ConversationWindow",
   components: {
@@ -109,6 +119,21 @@ export default {
       return noMessages
         ? null
         : messages[messages.length - 1].suggestedActions?.actions;
+    });
+    const messageTimestamp = computed(() => {
+      const msgs = chatMessage.value.messages;
+      if (msgs?.length >= 1) {
+        //create a timestamp using the oldest message sent by the bot
+        return new Date(msgs[0].timestamp).toLocaleString(
+          useI18n().locale.value,
+          {
+            weekday: "short",
+            hour: "2-digit",
+            minute: "2-digit",
+          }
+        );
+      }
+      return ""; //return nothing if no messages exist
     });
 
     function sendChatMessage(msg) {
@@ -175,6 +200,7 @@ export default {
       conversationWindow,
       focusOnInput,
       resetConversationWindow,
+      messageTimestamp,
     };
   },
 };
