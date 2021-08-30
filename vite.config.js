9e6deb5d4be8c70c4a1d836866a3bf3c40083720
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-08-30 12:53:17 -0400

    Process fix attempt 2?

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import path from 'path'
-import webpack from 'webpack'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +9,6 @@ export default defineConfig({
     vue(),
     vueI18n({
       include: path.resolve(__dirname, './src/locales/**'),
-    }) ,
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed)
-   })
+    })
   ]
 })
