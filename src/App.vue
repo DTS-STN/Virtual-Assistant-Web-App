aea18ba5b41ef3df14d64aeeccff59c256acd789
Kanmi Obasa <kanmi@konfirmed.com>
2021-11-30 13:23:26 -0500

    AA new build
    ran lint

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,9 @@ export default {
   components: {},
 };
 window.adobeDataLayer = window.adobeDataLayer || [];
-    adobeDataLayer.push({
-      'event': 'pageload',
-      'page' : document.title,
-      'dcterms.language': document
-      .getElementsByTagName("meta")
-      ["dcterms.language"]
-    });
+adobeDataLayer.push({
+  event: "pageload",
+  page: document.title,
+  "dcterms.language": document.getElementsByTagName("meta")["dcterms.language"],
+});
 </script>
