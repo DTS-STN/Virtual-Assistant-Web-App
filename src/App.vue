3f642adbbf901b0774a8dc3790c50b53ddf96c67
Kanmi Obasa <kanmi@konfirmed.com>
2021-11-30 15:58:28 -0500

    removed meta in html

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,4 @@ export default {
   name: "App",
   components: {},
 };
-window.adobeDataLayer = window.adobeDataLayer || [];
-adobeDataLayer.push({
-  event: "pageload",
-  page: {
-    title: document.title,
-    language: document.getElementsByTagName("meta")["dcterms.language"],
-    creator:
-      "Employment and Social Development Canada/Emploi et Développement social Canada",
-    accessRights: "2",
-    service: "ESDC-EDSC_DC-CD",
-  },
-});
 </script>
