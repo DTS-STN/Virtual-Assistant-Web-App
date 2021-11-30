1d9b757431a9414f54eef0ddebdaef0d882199fd
Kanmi Obasa <kanmi@konfirmed.com>
2021-11-30 13:20:39 -0500

    AA update
    Moved AA datalayer code from html to app.vue, added event, page name and dcterm meta

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@ export default {
   components: {},
 };
 window.adobeDataLayer = window.adobeDataLayer || [];
-adobeDataLayer.push({
-  event: "pageload",
-});
+    adobeDataLayer.push({
+      'event': 'pageload',
+      'page' : document.title,
+      'dcterms.language': document
+      .getElementsByTagName("meta")
+      ["dcterms.language"]
+    });
 </script>
